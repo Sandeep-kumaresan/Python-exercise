6bd7c95a4b5b3dfb0bf200f320f42a63ed9928c6
Sandeep K <sandeepkumaresan4676@gmail.com>
2024-07-09 17:39:49 +0530

    3rd advanced oops

--- 12.Advanced OOPS/3.stack.py
+++ 12.Advanced OOPS/3.stack.py
@@ -1,10 +1,25 @@
 class stack:
     def __init__(self):
         self.st=[]
-    def push(self):
-        self.st.append()
+    def push(self,i):
+        self.st.append(i)
     def pop1(self):
-        if not len(self.st)==0:
-            self.st.pop()
+        if not len(self.st) == 0:
+            return self.st.pop()
         else:
-            print("Error")
+            print("Error,Stack is empty")
+            return None
+    def peek(self):
+        if not len(self.st) == 0:
+            return self.st[-1]
+        else:
+            return "stack empty"
+
+stack1= stack()
+stack1.push(1)
+stack1.push(2)
+stack1.push(3)
+print(stack1.st)
+stack1.pop1()
+print(stack1.st)
+print(stack1.peek())
