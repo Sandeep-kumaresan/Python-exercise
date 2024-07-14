c097a80163bf7c51a74698ebd30a913874a6886c
Sandeep K <sandeepkumaresan4676@gmail.com>
2024-07-14 15:45:47 +0530

    roman to num update

--- Solving/Roman to number.py
+++ Solving/Roman to number.py
@@ -1,8 +1,8 @@
 roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-i="MDC"
+i="III"
 r=0
 for n in range(len(i)):
-    if roman[i[n]] <roman[i[n]]+1 and roman[i[n]] < len(i):
+    if n < len(i)-1 and roman[i[n]] <roman[i[n+1]] :
         r-=roman[i[n]]
     else:
         r+=roman[i[n]]
